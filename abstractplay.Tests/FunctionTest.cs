e7483767c50eb8b3943bd0f3d131a21dfd8b32b8
Aaron Dalton <aaron@daltons.ca>
2018-05-22 16:00:05 -0600

    Now connects to database. Working in testing environment, but not yet in production. Removing 'aws-lambda-tools-defaults.json' file for now because it has credentials.

--- abstractplay.Tests/FunctionTest.cs
+++ abstractplay.Tests/FunctionTest.cs
@@ -7,6 +7,7 @@ using Xunit;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
 using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
 
 using abstractplay;
 
@@ -19,7 +20,7 @@ namespace abstractplay.Tests
         }
 
         [Fact]
-        public void TetGetMethod()
+        public void TestRootGetMethod()
         {
             TestLambdaContext context;
             APIGatewayProxyRequest request;
@@ -30,9 +31,91 @@ namespace abstractplay.Tests
 
             request = new APIGatewayProxyRequest();
             context = new TestLambdaContext();
-            response = functions.Get(request, context);
+            response = functions.RootGet(request, context);
             Assert.Equal(200, response.StatusCode);
             Assert.Equal("Hello AWS Serverless", response.Body);
+        }
+
+        [Fact]
+        public void TestGuid()
+        {
+            HashSet<byte[]> guids = new HashSet<byte[]>();
+            Dictionary<string, int> guidlist = new Dictionary<string, int>();
+            int maxnum = 100;
+            for (int i=0; i<maxnum; i++)
+            {
+                byte[] g = GuidGenerator.GenerateSequentialGuid();
+                guids.Add(g);
+                guidlist[GuidGenerator.HelperBAToString(g)] = i;
+            }
+            Assert.Equal(maxnum, guids.Count);
+            List<string> keys = guidlist.Keys.ToList();
+            keys.Sort();
+            for (int i=1; i<keys.Count; i++)
+            {
+                Assert.Equal(1, guidlist[keys[i]] - guidlist[keys[i - 1]]);
+            }
+        }
+
+        [Fact]
+        public void TestUsersPost()
+        {
+            TestLambdaContext context;
+            APIGatewayProxyRequest request;
+            APIGatewayProxyResponse response;
+
+            Functions functions = new Functions();
+
+            Guid sub = new Guid("f686ace4-1946-4296-a4ff-6191d7e99004");
+
+            context = new TestLambdaContext();
+            request = new APIGatewayProxyRequest();
+            request.RequestContext = new APIGatewayProxyRequest.ProxyRequestContext();
+            request.RequestContext.Authorizer = new APIGatewayCustomAuthorizerContext();
+            request.RequestContext.Authorizer.Claims = new Dictionary<string, string>() { { "sub", "f686ace4-1946-4296-a4ff-6191d7e99004" } };
+
+            //Complete and error free
+            string Complete_ErrorFree = "{\"displayName\": \"Perlkönig\", \"country\": \"ca\", \"tagline\": \"Lasciate ogni speranza, voi ch'entrate!\", \"anonymous\": false, \"consent\": true}";
+            string Minimum_ErrorFree = "{\"displayName\": \"Perlkönig\", \"consent\": true}";
+            string MissingName = "{\"country\": \"ca\", \"tagline\": \"Lasciate ogni speranza, voi ch'entrate!\", \"anonymous\": false, \"consent\": true}";
+            string LongName = "{\"displayName\": \"0123456789012345678901234567890\", \"country\": \"ca\", \"tagline\": \"Lasciate ogni speranza, voi ch'entrate!\", \"anonymous\": false, \"consent\": true}";
+            string ShortName = "{\"displayName\": \"01\", \"country\": \"ca\", \"tagline\": \"Lasciate ogni speranza, voi ch'entrate!\", \"anonymous\": false, \"consent\": true}";
+            string MissingConsent = "{\"displayName\": \"Perlkönig\"}";
+            string FalseConsent = "{\"displayName\": \"Perlkönig\", \"consent\": false}";
+
+            request.Body = MissingName;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(400, response.StatusCode);
+            request.Body = LongName;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(400, response.StatusCode);
+            request.Body = ShortName;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(400, response.StatusCode);
+            request.Body = MissingConsent;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(400, response.StatusCode);
+            request.Body = FalseConsent;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(400, response.StatusCode);
+            request.Body = Complete_ErrorFree;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(201, response.StatusCode);
+            response = functions.UsersPost(request, context);
+            Assert.Equal(400, response.StatusCode);
+
+            //Need to delete entries now
+            MyContext dbc = new MyContext();
+            Owner ret = dbc.Owners.Where(x => x.CognitoId.Equals(sub.ToByteArray())).ToList()[0];
+            dbc.Remove(ret);
+            dbc.SaveChanges();
+
+            request.Body = Minimum_ErrorFree;
+            response = functions.UsersPost(request, context);
+            Assert.Equal(201, response.StatusCode);
+            ret = dbc.Owners.Where(x => x.CognitoId.Equals(sub.ToByteArray())).ToList()[0];
+            dbc.Remove(ret);
+            dbc.SaveChanges();
         }
     }
 }
--- abstractplay/Function.cs
+++ abstractplay/Function.cs
@@ -6,19 +6,50 @@ using System.Threading.Tasks;
 
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Diagnostics;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
 namespace abstractplay
 {
+    public struct ResponseError
+    {
+        public string request;
+        public string message;
+    }
+
+    public struct NameHistory
+    {
+        public string name;
+        public string effective_date;
+    }
+
+    public struct ResponseUser
+    {
+        public string id;
+        public string name;
+        public string country;
+        public string member_since;
+        public string tagline;
+        public List<NameHistory> name_history;
+    }
+
     public class Functions
     {
+        private readonly string SCHEMA_USER = "https://www.abstractplay.com/schemas/resources_user/1-0-0.json";
+
+        private static MyContext dbc;
+
         /// <summary>
         /// Default constructor that Lambda will invoke.
         /// </summary>
         public Functions()
         {
+            dbc = new MyContext();
+            dbc.Database.EnsureCreated();
         }
 
 
@@ -27,15 +58,282 @@ namespace abstractplay
         /// </summary>
         /// <param name="request"></param>
         /// <returns>The list of blogs</returns>
-        public APIGatewayProxyResponse Get(APIGatewayProxyRequest request, ILambdaContext context)
+        public APIGatewayProxyResponse RootGet(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            context.Logger.LogLine("Get Request\n");
-
             var response = new APIGatewayProxyResponse
             {
                 StatusCode = (int)HttpStatusCode.OK,
                 Body = "Hello AWS Serverless",
-                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } }
+                Headers = new Dictionary<string, string> { { "Content-Type", "text/plain; charset=utf-8" } }
+            };
+
+            return response;
+        }
+
+        public APIGatewayProxyResponse UsersPost(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            APIGatewayProxyResponse response = new APIGatewayProxyResponse();
+            dynamic body = JsonConvert.DeserializeObject(request.Body);
+
+            //displayName is required
+            string name;
+            StringInfo strinfo;
+            try
+            {
+                name = (string)body.displayName.ToObject(typeof(string));
+                strinfo = new StringInfo(name);
+            } catch (Exception e)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = "The 'displayName' field is required."
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+
+            //displayName must be between 3 and 30 characters
+            if ( (strinfo.LengthInTextElements < 3) || (strinfo.LengthInTextElements > 30) )
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = "The 'displayName' field must be between 3 and 30 utf-8 characters in length."
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+
+            //Country Code
+            string country = null;
+            try
+            {
+                country = (string)body.country.ToObject(typeof(string));
+                country = country.ToUpper();
+            }
+            catch (Exception e)
+            {
+                //Do nothing because "country" defaults to null.
+                //Ideally we'd check that a property exists, but we can't do that with dynamic variables.
+            }
+
+            if (country != null)
+            {
+                country = (string)body.country.ToObject(typeof(string));
+                country = country.ToUpper();
+                if (country.Length != 2)
+                {
+                    ResponseError r = new ResponseError()
+                    {
+                        request = JsonConvert.SerializeObject(body),
+                        message = "The 'country' field must consist of only two characters, representing a valid ISO 3166-1 alpha-2 country code."
+                    };
+                    response = new APIGatewayProxyResponse
+                    {
+                        StatusCode = (int)HttpStatusCode.BadRequest,
+                        Body = JsonConvert.SerializeObject(r),
+                        Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                    };
+                    return response;
+                }
+            }
+
+            //Tagline
+            string tagline = null;
+            try
+            {
+                tagline = (string)body.tagline.ToObject(typeof(string));
+                strinfo = new StringInfo(tagline);
+            } catch (Exception e)
+            {
+                //Do nothing because "tagline" defaults to null.
+                //Ideally we'd check that a property exists, but we can't do that with dynamic variables.
+            }
+            if (tagline != null)
+            {
+                tagline = (string)body.tagline.ToObject(typeof(string));
+                strinfo = new StringInfo(tagline);
+                if (strinfo.LengthInTextElements > 255)
+                {
+                    ResponseError r = new ResponseError()
+                    {
+                        request = JsonConvert.SerializeObject(body),
+                        message = "The 'tagline' field may not exceed 255 characters in length."
+                    };
+                    response = new APIGatewayProxyResponse
+                    {
+                        StatusCode = (int)HttpStatusCode.BadRequest,
+                        Body = JsonConvert.SerializeObject(r),
+                        Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                    };
+                    return response;
+                }
+            }
+
+            //Anonymous?
+            bool anon = false;
+            try
+            {
+                anon = (bool)body.anonymous.ToObject(typeof(bool));
+            } catch (Exception e)
+            {
+                //Do nothing because "anon" defaults to null.
+                //Ideally we'd check that a property exists, but we can't do that with dynamic variables.
+            }
+
+            //Consent
+            bool consent = false;
+            try
+            {
+                consent = (bool)body.consent.ToObject(typeof(bool));
+            } catch (Exception e)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = "You must consent to the terms of service and to the processing of your data to create an account and use Abstract Play."
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+
+            if (! consent)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = "You must consent to the terms of service and to the processing of your data to create an account and use Abstract Play."
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+
+            //Check for duplicate cognito ID
+            if (request.RequestContext.Authorizer.Claims["sub"] == null)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = "You do not appear to be logged in! Nobody should ever see this message!"
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.Unauthorized,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+            Guid cognitoId = new Guid(request.RequestContext.Authorizer.Claims["sub"]);
+            var rets = dbc.Owners.Where(x => x.CognitoId.Equals(cognitoId.ToByteArray())).ToList();
+            if (rets.Count > 0)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = "You already have an account (ID "+ GuidGenerator.HelperBAToString(rets[0].OwnerId) +"). Having multiple accounts if forbidden."
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.BadRequest,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+
+            //All is well! Create the object.
+            Owner owner;
+            NameEntry ne;
+            byte[] ownerId = GuidGenerator.GenerateSequentialGuid();
+            try
+            {
+                Guid playerId = Guid.NewGuid();
+                DateTime now = DateTime.UtcNow;
+
+                ne = new NameEntry { EntryId = GuidGenerator.GenerateSequentialGuid(), OwnerId = ownerId, EffectiveFrom = now, Name = name};
+                owner = new Owner { OwnerId = ownerId, CognitoId = cognitoId.ToByteArray(), PlayerId = playerId.ToByteArray(), DateCreated = now, ConsentDate = now, Anonymous = anon, Country = country, Tagline = tagline};
+                dbc.Add(owner);
+                dbc.Add(ne);
+                dbc.SaveChanges();
+            } catch (Exception e)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = e.ToString()
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+
+            //Refetch the object to make sure we're returning canonical data
+            Owner ret;
+            try
+            {
+                ret = dbc.Owners.Where(x => x.OwnerId.Equals(ownerId)).ToList()[0];
+            } catch (Exception e)
+            {
+                ResponseError r = new ResponseError()
+                {
+                    request = JsonConvert.SerializeObject(body),
+                    message = e.ToString()
+                };
+                response = new APIGatewayProxyResponse
+                {
+                    StatusCode = (int)HttpStatusCode.InternalServerError,
+                    Body = JsonConvert.SerializeObject(r),
+                    Headers = new Dictionary<string, string> { { "Content-Type", "application/json; charset=utf-8" } }
+                };
+                return response;
+            }
+            NameEntry activeName = ret.Names.ToArray()[0];
+
+            //Return the object.
+            ResponseUser ru = new ResponseUser()
+            {
+                id = GuidGenerator.HelperBAToString(ret.PlayerId),
+                name = activeName.Name,
+                country = ret.Country,
+                member_since = ret.DateCreated.ToString("o"),
+                tagline = ret.Tagline,
+                name_history = new List<NameHistory>() { new NameHistory() { name = activeName.Name, effective_date = activeName.EffectiveFrom.ToString("o") } }
+            };
+            response = new APIGatewayProxyResponse
+            {
+                StatusCode = (int)HttpStatusCode.Created,
+                Body = JsonConvert.SerializeObject(ru),
+                Headers = new Dictionary<string, string> {
+                    { "Content-Type", "text/plain; charset=utf-8" },
+                    { "Location", "/users/"+ GuidGenerator.HelperBAToString(ret.OwnerId) },
+                    { "Link", "<" + SCHEMA_USER + ">; rel=\"describedBy\"" }
+                }
             };
 
             return response;
