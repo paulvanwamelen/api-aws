4a091f5305d1884e63823c0f1d3af85da847c38a
Aaron Dalton <aaron@daltons.ca>
2018-07-15 14:10:28 -0600

    Case in point. Forgot manual insertion of date conversion.

--- sln/abstractplay/DB/MyContext.cs
+++ sln/abstractplay/DB/MyContext.cs
@@ -627,6 +627,17 @@ namespace abstractplay.DB
                     .HasForeignKey(d => d.OwnerId)
                     .HasConstraintName("fk_name2owner");
             });
+            var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+                v => v, v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime) || property.ClrType == typeof(DateTime?))
+                        property.SetValueConverter(dateTimeConverter);
+                }
+            }
         }
     }
 }
